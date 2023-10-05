5c70141eb928752bb4d62bac11bfb42a092b581a
NgTrHau <92238714+NgTrHau@users.noreply.github.com>
2023-10-05 17:13:59 +0700

    Create model, controller, route of comment in server

--- server/app.js
+++ server/app.js
@@ -1,34 +1,36 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const ApiError = require("./app/api-error");
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const ApiError = require('./app/api-error');
 
 const app = express();
 
-const userRouter = require("./app/routes/user.route");
-const postRouter = require("./app/routes/post.route");
-const artRouter = require("./app/routes/art.route");
+const userRouter = require('./app/routes/user.route');
+const postRouter = require('./app/routes/post.route');
+const artRouter = require('./app/routes/art.route');
+const commentRouter = require('./app/routes/comment.route');
 
 app.use(cors());
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to PixelLand application." });
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to PixelLand application.' });
 });
 
-app.use("/api/users", userRouter);
-app.use("/api/posts", postRouter);
-app.use("/api/arts", artRouter);
+app.use('/api/users', userRouter);
+app.use('/api/posts', postRouter);
+app.use('/api/arts', artRouter);
+app.use('/api/comments', commentRouter);
 
 // handle 404 response
 app.use((req, res, next) => {
-  return next(new ApiError(404, "Resource not found"));
+  return next(new ApiError(404, 'Resource not found'));
 });
 
 app.use((ApiError, req, res, next) => {
   return res.status(ApiError.statusCode || 500).json({
-    message: ApiError.message || "Internal Server Error",
+    message: ApiError.message || 'Internal Server Error',
   });
 });
 
