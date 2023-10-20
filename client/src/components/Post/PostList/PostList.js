1f270e29b8b934736e31566dbf1942e40614d2f3
NgTrHau <92238714+NgTrHau@users.noreply.github.com>
2023-10-20 22:33:23 +0700

    Build loading posts UI

--- client/src/components/Post/PostList/PostList.js
+++ client/src/components/Post/PostList/PostList.js
@@ -19,6 +19,25 @@ function PostList() {
   const { posts, isPostsLoading, isError, message } = useSelector(
     (state) => state.posts,
   );
+
+  const cards_sample = Array(2)
+    .fill(undefined)
+    .map((a, i) => (
+      <div className={cx('post-thumb-sample')} key={i}>
+        <div className={cx('post-user-sample')}>
+          <div className={cx('post-avatar-sample')}></div>
+          <div className={cx('post-username-sample')}>
+            <div className={cx('post-username-sample-1')}></div>
+            <div className={cx('post-username-sample-2')}></div>
+          </div>
+        </div>
+        <div className={cx('post-actions-sample')}>
+          <div className={cx('post-action-sample')}></div>
+          <div className={cx('post-action-sample')}></div>
+          <div className={cx('post-action-sample')}></div>
+        </div>
+      </div>
+    ));
   useEffect(() => {
     if (isError) {
       console.log(message);
@@ -35,11 +54,10 @@ function PostList() {
   if (!user) {
     return null;
   }
-
   return (
     <div className={cx('wrapper')}>
       {isPostsLoading ? (
-        <SpinIcon />
+        cards_sample
       ) : (
         <section className={cx('content')}>
           {posts.length > 0 ? (
